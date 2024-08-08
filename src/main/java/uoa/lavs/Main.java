d4dbd526c2cc362720895fb28952f06f4a640d35
victor <vqiu25@gmail.com>
2024-08-08 23:05:41 +1200

    Feature: JavaFX Window Creation

--- src/main/java/uoa/lavs/Main.java
+++ src/main/java/uoa/lavs/Main.java
@@ -7,32 +7,44 @@ import uoa.lavs.mainframe.messages.customer.LoadCustomer;
 import uoa.lavs.mainframe.simulator.RecorderConnection;
 import uoa.lavs.mainframe.simulator.SimpleReplayConnection;
 
+import javafx.application.Application;
+import javafx.stage.Stage;
+
 import java.io.IOException;
 
-public class Main {
+public class Main extends Application {
+
     public static void main(String[] args) {
-        // the following shows two ways of using the mainframe interface
-        // approach #1: use the singleton instance - this way is recommended as it provides a single configuration
+        // Launch the JavaFX application
+        launch(args);
+
+        // The following shows two ways of using the mainframe interface
+        // Approach #1: Use the singleton instance - this way is recommended as it provides a single configuration
         // location (and is easy for the testers to change when needed).
         Connection connection = Instance.getConnection();
         executeTestMessage(connection);
 
-        // approach #2: dynamically initialize the interface based on some parameters - this way allows the connection
+        // Approach #2: Dynamically initialize the interface based on some parameters - this way allows the connection
         // to change when needed (e.g., based on a command-line argument.) But it means that the connection must be
         // passed around in the application.
         String dataPath = args.length > 1 ? args[1] : "lavs-data.txt";
-        if (args.length > 0 && args[0].equals("record"))
-        {
+        if (args.length > 0 && args[0].equals("record")) {
             connection = new RecorderConnection(dataPath);
-        }
-        else
-        {
+        } else {
             connection = new SimpleReplayConnection(dataPath);
         }
         executeTestMessage(connection);
 
-        // you can use another approach if desired, but make sure you document how the markers can change the
+        // You can use another approach if desired, but make sure you document how the markers can change the
         // connection implementation.
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("JavaFX Application");
+        primaryStage.setWidth(400);
+        primaryStage.setHeight(300);
+        primaryStage.show();
     }
 
     private static void executeTestMessage(Connection connection) {
@@ -42,7 +54,7 @@ public class Main {
         try {
             connection.close();
         } catch (IOException e) {
-            System.out.println("Something went wrong - could not clos connection! The message is " + e.getMessage());
+            System.out.println("Something went wrong - could not close connection! The message is " + e.getMessage());
             return;
         }
 
