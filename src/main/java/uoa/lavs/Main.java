706a4e700c629e66f8fb0292875ccf70c1169460
Jess <126314571+jj2947@users.noreply.github.com>
2024-08-09 15:07:58 +1200

    Revert "Feature: added initial template code for javafx"

--- src/main/java/uoa/lavs/Main.java
+++ src/main/java/uoa/lavs/Main.java
@@ -33,7 +33,6 @@ public class Main {
 
         // you can use another approach if desired, but make sure you document how the markers can change the
         // connection implementation.
-        App.startApp();
     }
 
     private static void executeTestMessage(Connection connection) {
