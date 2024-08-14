98654016b8435cce6937b16283ac52c6a809a431
Eason <ejin458@aucklanduni.ac.nz>
2024-08-14 17:29:18 +1200

    :sparkles: Feature: clearing fields on cancel and displaying alert

--- src/main/java/uoa/lavs/controllers/AddCustomerController.java
+++ src/main/java/uoa/lavs/controllers/AddCustomerController.java
@@ -1,9 +1,13 @@
 package uoa.lavs.controllers;
 
+import java.io.IOException;
 import javafx.collections.FXCollections;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
 import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
 import javafx.scene.control.CheckBox;
 import javafx.scene.control.ComboBox;
 import javafx.scene.control.DatePicker;
@@ -11,79 +15,124 @@ import javafx.scene.control.TextField;
 import uoa.lavs.Main;
 import uoa.lavs.SceneManager;
 import uoa.lavs.dataoperations.CustomerUpdater;
-import uoa.lavs.mainframe.Connection;
-import uoa.lavs.mainframe.Instance;
-import uoa.lavs.mainframe.messages.customer.LoadCustomer;
 import uoa.lavs.models.Customer;
-import atlantafx.base.theme.Styles;
-
-
-import java.io.IOException;
 
 public class AddCustomerController {
-    
-    @FXML private Button homeButton;
-    @FXML private Button saveButton;
-    @FXML private Button cancelButton;
-    @FXML private Button infoButton;
 
-    @FXML private TextField titleField;
-    @FXML private TextField familyNameField;
-    @FXML private TextField givenNameField;
-    @FXML private DatePicker dobPicker;
+  @FXML private Button homeButton;
+  @FXML private Button saveButton;
+  @FXML private Button cancelButton;
+  @FXML private Button infoButton;
 
-    @FXML private TextField citizenshipField;
-    @FXML private TextField visaField;
+  @FXML private TextField titleField;
+  @FXML private TextField familyNameField;
+  @FXML private TextField givenNameField;
+  @FXML private DatePicker dobPicker;
 
-    @FXML private TextField address1Field;
-    @FXML private TextField address2Field;
-    @FXML private TextField suburbField;
-    @FXML private TextField cityField;
-    @FXML private TextField postcodeField;
-    @FXML private TextField countryField;
-    @FXML private CheckBox isPrimaryAddress;
-    @FXML private CheckBox isMailingAddress;
+  @FXML private TextField citizenshipField;
+  @FXML private TextField visaField;
 
-    @FXML private TextField emailField;
-    @FXML private CheckBox isPrimaryEmail;
-    @FXML private ComboBox<FXCollections> phoneTypeBox;
-    @FXML private TextField prefixField;
-    @FXML private TextField numberField;
-    @FXML private CheckBox isPrimaryNumber;
-    @FXML private CheckBox isTextingNumber;
+  @FXML private TextField address1Field;
+  @FXML private TextField address2Field;
+  @FXML private TextField suburbField;
+  @FXML private TextField cityField;
+  @FXML private TextField postcodeField;
+  @FXML private TextField countryField;
+  @FXML private CheckBox isPrimaryAddress;
+  @FXML private CheckBox isMailingAddress;
 
-    @FXML private TextField jobField;
-    @FXML private TextField companyNameField;
-    @FXML private TextField companyAddress1Field;
-    @FXML private TextField companyAddress2Field;
-    @FXML private TextField companySuburbField;
-    @FXML private TextField companyCityField;
-    @FXML private TextField companyPostcodeField;
-    @FXML private TextField companyCountryField;
-    @FXML private TextField employerPhoneField;
-    @FXML private TextField employerEmailField;
-    @FXML private TextField companyWebsiteField;
+  @FXML private TextField emailField;
+  @FXML private CheckBox isPrimaryEmail;
+  @FXML private ComboBox<FXCollections> phoneTypeBox;
+  @FXML private TextField prefixField;
+  @FXML private TextField numberField;
+  @FXML private CheckBox isPrimaryNumber;
+  @FXML private CheckBox isTextingNumber;
 
-    @FXML
-    private void onClickHome(ActionEvent event) throws IOException {
-        Main.setScene(SceneManager.AppScene.START);
+  @FXML private TextField jobField;
+  @FXML private TextField companyNameField;
+  @FXML private TextField companyAddress1Field;
+  @FXML private TextField companyAddress2Field;
+  @FXML private TextField companySuburbField;
+  @FXML private TextField companyCityField;
+  @FXML private TextField companyPostcodeField;
+  @FXML private TextField companyCountryField;
+  @FXML private TextField employerPhoneField;
+  @FXML private TextField employerEmailField;
+  @FXML private TextField companyWebsiteField;
+
+  @FXML
+  private void onClickHome(ActionEvent event) throws IOException {
+    Main.setScene(SceneManager.AppScene.START);
+  }
+
+  @FXML
+  private void onClickSave(ActionEvent event) {
+    // Save the form to the mainframe
+    Customer customer =
+        new Customer(
+            null,
+            titleField.getText(),
+            givenNameField.getText() + " " + familyNameField.getText(),
+            dobPicker.getValue(),
+            jobField.getText(),
+            citizenshipField.getText(),
+            visaField.getText(),
+            "Active");
+    CustomerUpdater updater = new CustomerUpdater();
+    updater.updateData(null, customer);
+  }
+
+  @FXML
+  private void onClickCancel(ActionEvent event) {
+    Alert alert = new Alert(AlertType.CONFIRMATION);
+    alert.setTitle("Cancel adding customer");
+    alert.setHeaderText("If you cancel, all progress will be lost.");
+    alert.setContentText("Are you sure you want to cancel?");
+    if (alert.showAndWait().get() == ButtonType.OK) {
+      clearAllFields();
+      Main.setScene(SceneManager.AppScene.START);
     }
+  }
 
-    @FXML
-    private void onClickSave(ActionEvent event) {
-        // Save the form to the mainframe
-        Customer customer = new Customer(null, titleField.getText(), givenNameField.getText() + " " + familyNameField.getText(), dobPicker.getValue(), jobField.getText(), citizenshipField.getText(), visaField.getText(), "Active");
-        CustomerUpdater updater = new CustomerUpdater();
-        updater.updateData(null, customer);
-    }
+  @FXML
+  private void onClickInfo(ActionEvent event) {
+    System.out.println("Info button clicked");
+  }
 
-    @FXML
-    private void onClickCancel(ActionEvent event) {
-        
-    }
-
-    @FXML
-    private void onClickInfo(ActionEvent event) {
-        // Show the help dialog
-    }
+  private void clearAllFields() {
+    titleField.clear();
+    familyNameField.clear();
+    givenNameField.clear();
+    dobPicker.setValue(null);
+    jobField.clear();
+    citizenshipField.clear();
+    visaField.clear();
+    address1Field.clear();
+    address2Field.clear();
+    suburbField.clear();
+    cityField.clear();
+    postcodeField.clear();
+    countryField.clear();
+    isPrimaryAddress.setSelected(false);
+    isMailingAddress.setSelected(false);
+    emailField.clear();
+    isPrimaryEmail.setSelected(false);
+    phoneTypeBox.setValue(null);
+    prefixField.clear();
+    numberField.clear();
+    isPrimaryNumber.setSelected(false);
+    isTextingNumber.setSelected(false);
+    jobField.clear();
+    companyNameField.clear();
+    companyAddress1Field.clear();
+    companyAddress2Field.clear();
+    companySuburbField.clear();
+    companyCityField.clear();
+    companyPostcodeField.clear();
+    companyCountryField.clear();
+    employerPhoneField.clear();
+    employerEmailField.clear();
+    companyWebsiteField.clear();
+  }
 }
