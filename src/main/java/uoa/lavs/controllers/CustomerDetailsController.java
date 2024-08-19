56e45cdec1d623bb1fc29b8a39cf96c2ca0e4d4b
LiamParker1 <lpar161@aucklanduni.ac.nz>
2024-08-19 22:58:40 +1200

    Bugfix: Add Controller annotation to CustomerDetailsController

--- src/main/java/uoa/lavs/controllers/CustomerDetailsController.java
+++ src/main/java/uoa/lavs/controllers/CustomerDetailsController.java
@@ -2,12 +2,15 @@ package uoa.lavs.controllers;
 
 import java.io.IOException;
 
+import org.springframework.stereotype.Controller;
+
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import uoa.lavs.Main;
 import uoa.lavs.SceneManager.AppScene;
 
+@Controller
 public class CustomerDetailsController {
     @FXML private Button backButton;
 
