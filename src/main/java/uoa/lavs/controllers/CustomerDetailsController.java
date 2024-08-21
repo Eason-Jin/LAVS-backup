49bf7cb685e9539181de8c604d9d4c30083463ed
LiamParker1 <lpar161@aucklanduni.ac.nz>
2024-08-21 17:16:52 +1200

    Feature: Create base UI for customer details

--- src/main/java/uoa/lavs/controllers/CustomerDetailsController.java
+++ src/main/java/uoa/lavs/controllers/CustomerDetailsController.java
@@ -13,9 +13,31 @@ import uoa.lavs.SceneManager.AppScene;
 @Controller
 public class CustomerDetailsController {
     @FXML private Button backButton;
+    @FXML private Button homeButton;
+    @FXML private Button infoButton;
+    @FXML private Button editButton;
+
+    public void setCustomerDetails(String customerId) {
+
+    }
 
     @FXML
     private void onClickBack(ActionEvent event) throws IOException {
         Main.setScene(AppScene.SEARCH);
+    }
+
+    @FXML
+    private void onClickHome(ActionEvent event) throws IOException {
+        Main.setScene(AppScene.START);
+    }
+
+    @FXML
+    private void onClickInfo(ActionEvent event) {
+        System.out.println("Info button clicked");
+    }
+
+    @FXML
+    private void onClickEdit(ActionEvent event) {
+        System.out.println("Edit button clicked");
     }
 }
