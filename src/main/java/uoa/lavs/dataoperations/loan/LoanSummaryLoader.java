74d8bacef2a8eea877d4eb37f4077aa13f343a51
victor <vqiu25@gmail.com>
2024-08-22 11:19:48 +1200

    Refactor: Adjust method signature to be private

--- src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
+++ src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
@@ -1,43 +1,42 @@
 package uoa.lavs.dataoperations.loan;
 
-import uoa.lavs.mainframe.Frequency;
 import uoa.lavs.mainframe.Instance;
-import uoa.lavs.mainframe.RateType;
 import uoa.lavs.mainframe.Status;
 import uoa.lavs.mainframe.messages.loan.LoadLoanSummary;
-import uoa.lavs.models.Loan;
 import uoa.lavs.utility.LoanRepayment;
 import uoa.lavs.utility.LoanSummary;
-import uoa.lavs.utility.PaymentFrequency;
 
-import java.sql.Connection;
-import java.sql.PreparedStatement;
-import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoanSummaryLoader {
 
-    public static LoanSummary calculateLocally(String loanId) throws Exception {
-        LoanPaymentsLoader loanPaymentsLoader = new LoanPaymentsLoader();
-        List<LoanRepayment> loanRepayments = loanPaymentsLoader.calculateLocally(loanId);
-        String query = "SELECT * FROM Loan WHERE LoanID = ?";
-        try (Connection connection = Instance.getDatabaseConnection();
-             PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, loanId);
-            try (ResultSet resultSet = statement.executeQuery()) {
-                if (!resultSet.next()) {
-                    throw new Exception("Loan not in database");
-                }
-                Frequency paymentFrequency = Frequency.valueOf(resultSet.getString("PaymentFrequency"));
-                LoanSummary loanSummary = new LoanSummary((ArrayList<LoanRepayment>) loanRepayments, paymentFrequency);
-                return loanSummary;
+    public static LoanSummary calculateLoanSummary(String loanId) {
+        LoanSummary loanSummary = new LoanSummary();
+        try {
+            loanSummary = calculateFromMainframe(loanId);
+        } catch (Exception e) {
+            System.out.println("Mainframe calculation failed: " + e.getMessage());
+            System.out.println("Trying to find and calculate from database");
+            try {
+                loanSummary = calculateLocally(loanId);
+            } catch (Exception e1) {
+                System.out.println("Database find and calculation failed: " + e1.getMessage());
             }
         }
+        return loanSummary;
+
     }
 
-    public static LoanSummary calculateFromMainframe(String loanId) throws Exception {
+    private static LoanSummary calculateLocally(String loanId) throws Exception {
+        LoanPaymentsLoader loanPaymentsLoader = new LoanPaymentsLoader();
+        List<LoanRepayment> loanRepayments = loanPaymentsLoader.calculateLocally(loanId);
+        LoanSummary loanSummary = new LoanSummary((ArrayList<LoanRepayment>) loanRepayments);
+        return loanSummary;
+    }
+
+    private static LoanSummary calculateFromMainframe(String loanId) throws Exception {
         LoadLoanSummary loadLoanSummary = new LoadLoanSummary();
         loadLoanSummary.setLoanId(loanId);
         Status status = loadLoanSummary.send(Instance.getConnection());
