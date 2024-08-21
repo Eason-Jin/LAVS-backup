d707d9153356b22265dc785a89fb889ecf8a7c8f
victor <vqiu25@gmail.com>
2024-08-22 10:39:31 +1200

    Feature: Only utilise local methods for local calculation

--- src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
+++ src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
@@ -1,23 +1,40 @@
 package uoa.lavs.dataoperations.loan;
 
+import uoa.lavs.mainframe.Frequency;
 import uoa.lavs.mainframe.Instance;
+import uoa.lavs.mainframe.RateType;
 import uoa.lavs.mainframe.Status;
 import uoa.lavs.mainframe.messages.loan.LoadLoanSummary;
+import uoa.lavs.models.Loan;
 import uoa.lavs.utility.LoanRepayment;
 import uoa.lavs.utility.LoanSummary;
+import uoa.lavs.utility.PaymentFrequency;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 public class LoanSummaryLoader {
 
-    public static LoanSummary calculateLocally(String loanId) {
+    public static LoanSummary calculateLocally(String loanId) throws Exception {
         LoanPaymentsLoader loanPaymentsLoader = new LoanPaymentsLoader();
-        List<LoanRepayment> loanRepayments = loanPaymentsLoader.calculateLoanRepayments(loanId);
-
-        LoanSummary loanSummary = new LoanSummary((ArrayList<LoanRepayment>) loanRepayments);
-        return loanSummary;
+        List<LoanRepayment> loanRepayments = loanPaymentsLoader.calculateLocally(loanId);
+        String query = "SELECT * FROM Loan WHERE LoanID = ?";
+        try (Connection connection = Instance.getDatabaseConnection();
+             PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, loanId);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                if (!resultSet.next()) {
+                    throw new Exception("Loan not in database");
+                }
+                Frequency paymentFrequency = Frequency.valueOf(resultSet.getString("PaymentFrequency"));
+                LoanSummary loanSummary = new LoanSummary((ArrayList<LoanRepayment>) loanRepayments, paymentFrequency);
+                return loanSummary;
+            }
+        }
     }
 
     public static LoanSummary calculateFromMainframe(String loanId) throws Exception {
