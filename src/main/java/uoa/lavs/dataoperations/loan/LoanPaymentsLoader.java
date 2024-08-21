10b0b6e53b3cb16dca68160630e6a88c9106e143
victor <vqiu25@gmail.com>
2024-08-21 18:36:34 +1200

    Bugfix: Load repayments from all pages instead of one

--- src/main/java/uoa/lavs/dataoperations/loan/LoanPaymentsLoader.java
+++ src/main/java/uoa/lavs/dataoperations/loan/LoanPaymentsLoader.java
@@ -3,8 +3,7 @@ package uoa.lavs.dataoperations.loan;
 import uoa.lavs.mainframe.Instance;
 import uoa.lavs.mainframe.Status;
 import uoa.lavs.mainframe.messages.loan.LoadLoanPayments;
-import uoa.lavs.models.Loan;
-import uoa.lavs.models.LoanPayment;
+import uoa.lavs.utility.LoanRepayment;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -12,11 +11,26 @@ import java.time.LocalDate;
 
 public class LoanPaymentsLoader {
 
-    private static List<LoanPayment> loadFromMainframe(String loanId, int number) throws Exception {
-        // Retrieve from mainframe
+//    private static List<LoanPayment> calculateLocally(String loanId, int number) throws Exception {
+//        // Calculate locally
+//        AmortizingLoanCalculator calculator = new AmortizingLoanCalculator();
+//        List<LoanPayment> loanPayments = new ArrayList<>();
+//
+//        // Load in the loan
+//        LoanFinder loanFinder = new LoanFinder();
+//
+//        loanPayments = calculator.generateRepaymentSchedule();
+//
+//        return loanPayments;
+//    }
+
+    public static List<LoanRepayment> calculateFromMainframe(String loanId) throws Exception {
+        // Initialise LoadLoanPayments message
         LoadLoanPayments loadLoanPayments = new LoadLoanPayments();
         loadLoanPayments.setLoanId(loanId);
-        loadLoanPayments.setNumber(number);
+
+        // Send request to get the first page to determine total page count
+        loadLoanPayments.setNumber(1);
         Status status = loadLoanPayments.send(Instance.getConnection());
         if (!status.getWasSuccessful()) {
             System.out.println(
@@ -24,15 +38,45 @@ public class LoanPaymentsLoader {
             throw new Exception("Mainframe send failed");
         }
 
-        // Initialising variables from output received from mainframe
-        Integer repaymentCount = loadLoanPayments.getPaymentCountFromServer();
-        List<LoanPayment> loanPayments = new ArrayList<>();
-        for (int i = 0; i < repaymentCount; i++) {
-            LocalDate month = loadLoanPayments.getPaymentDateFromServer(i);
-            Double principal = loadLoanPayments.getPaymentPrincipalFromServer(i);
-            Double interest = loadLoanPayments.getPaymentInterestFromServer(i);
-            Double remaining = loadLoanPayments.getPaymentRemainingFromServer(i);
-            loanPayments.add(new LoanPayment(month, principal, interest, remaining));
+        // Get the total number of pages
+        Integer pageCount = loadLoanPayments.getPageCountFromServer();
+        if (pageCount == null) {
+            throw new Exception("Failed to retrieve page count from server.");
+        }
+
+        List<LoanRepayment> loanPayments = new ArrayList<>();
+
+        // Loop over all pages
+        for (int pageNumber = 1; pageNumber <= pageCount; pageNumber++) {
+            loadLoanPayments.setNumber(pageNumber);
+            status = loadLoanPayments.send(Instance.getConnection());
+            if (!status.getWasSuccessful()) {
+                String errorMessage = "Failed to retrieve page " + pageNumber + ". Error code: " + status.getErrorCode();
+                System.out.println(errorMessage);
+                throw new Exception(errorMessage);
+            }
+
+            // Get the number of payments on this page
+            Integer paymentCount = loadLoanPayments.getPaymentCountFromServer();
+            if (paymentCount == null) {
+                System.out.println("No payments found on page " + pageNumber);
+                continue;
+            }
+
+            // Loop through each payment on this page and collect the details
+            for (int i = 1; i <= paymentCount; i++) {
+                Integer number = loadLoanPayments.getPaymentNumberFromServer(i);
+                LocalDate month = loadLoanPayments.getPaymentDateFromServer(i);
+                Double principal = loadLoanPayments.getPaymentPrincipalFromServer(i);
+                Double interest = loadLoanPayments.getPaymentInterestFromServer(i);
+                Double remaining = loadLoanPayments.getPaymentRemainingFromServer(i);
+
+                if (month != null && principal != null && interest != null && remaining != null) {
+                    loanPayments.add(new LoanRepayment(number, month, principal, interest, remaining));
+                } else {
+                    System.out.println("Warning: Skipping payment " + i + " on page " + pageNumber + " due to missing data.");
+                }
+            }
         }
 
         return loanPayments;
