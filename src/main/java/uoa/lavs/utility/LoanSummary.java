9dfc4cee1cdbd995fd8f8f999aa6a7fa90ecbbfd
victor <vqiu25@gmail.com>
2024-08-22 10:11:10 +1200

    Bugfix: Calculate payoff date correctly

--- src/main/java/uoa/lavs/utility/LoanSummary.java
+++ src/main/java/uoa/lavs/utility/LoanSummary.java
@@ -7,6 +7,7 @@ public class LoanSummary {
     private Double totalInterest = 0.0;
     private Double totalCost = 0.0;
     private LocalDate payOffDate;
+    private PaymentFrequency paymentFrequency;
 
     public LoanSummary(Double totalInterest, Double totalCost, LocalDate payOffDate) {
         this.totalInterest = totalInterest;
@@ -14,7 +15,7 @@ public class LoanSummary {
         this.payOffDate = payOffDate;
     }
 
-    public LoanSummary(ArrayList<LoanRepayment> repayments) {
+    public LoanSummary(ArrayList<LoanRepayment> repayments, PaymentFrequency paymentFrequency) {
 
         for (LoanRepayment repayment : repayments) {
             totalInterest += repayment.getInterestAmount();
@@ -22,7 +23,13 @@ public class LoanSummary {
         }
         totalCost += totalInterest;
 
-        this.payOffDate = repayments.get(repayments.size() - 1).getRepaymentDate();
+        // Adjust the payoff date based on the payment frequency
+        LocalDate lastRepaymentDate = repayments.get(repayments.size() - 1).getRepaymentDate();
+        switch (paymentFrequency) {
+            case Weekly -> this.payOffDate = lastRepaymentDate.plusWeeks(1);
+            case Fortnightly -> this.payOffDate = lastRepaymentDate.plusWeeks(2);
+            case Monthly -> this.payOffDate = lastRepaymentDate.plusMonths(1);
+        }
     }
 
     public Double getTotalInterest() {
