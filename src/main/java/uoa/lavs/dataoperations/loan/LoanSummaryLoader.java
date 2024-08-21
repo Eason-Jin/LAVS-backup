170a73dda248b34e4068ac05b3a5b755407449a4
victor <vqiu25@gmail.com>
2024-08-22 10:06:47 +1200

    Feature: Data operations for local and mainframe connection for loan summary

--- src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
+++ src/main/java/uoa/lavs/dataoperations/loan/LoanSummaryLoader.java
@@ -1,2 +1,38 @@
-package uoa.lavs.dataoperations.loan;public class LoanSummaryLoader {
+package uoa.lavs.dataoperations.loan;
+
+import uoa.lavs.mainframe.Instance;
+import uoa.lavs.mainframe.Status;
+import uoa.lavs.mainframe.messages.loan.LoadLoanSummary;
+import uoa.lavs.utility.LoanRepayment;
+import uoa.lavs.utility.LoanSummary;
+
+import java.time.LocalDate;
+import java.util.ArrayList;
+import java.util.List;
+
+public class LoanSummaryLoader {
+
+    public static LoanSummary calculateLocally(String loanId) {
+        LoanPaymentsLoader loanPaymentsLoader = new LoanPaymentsLoader();
+        List<LoanRepayment> loanRepayments = loanPaymentsLoader.calculateLoanRepayments(loanId);
+
+        LoanSummary loanSummary = new LoanSummary((ArrayList<LoanRepayment>) loanRepayments);
+        return loanSummary;
+    }
+
+    public static LoanSummary calculateFromMainframe(String loanId) throws Exception {
+        LoadLoanSummary loadLoanSummary = new LoadLoanSummary();
+        loadLoanSummary.setLoanId(loanId);
+        Status status = loadLoanSummary.send(Instance.getConnection());
+        if (!status.getWasSuccessful()) {
+            System.out.println(
+                    "Something went wrong - the Mainframe send failed! The code is " + status.getErrorCode());
+            throw new Exception("Mainframe send failed");
+        }
+        Double totalInterest = loadLoanSummary.getTotalInterestFromServer();
+        Double totalLoanCost = loadLoanSummary.getTotalLoanCostFromServer();
+        LocalDate payOffDate = loadLoanSummary.getPayoffDateFromServer();
+        LoanSummary loanSummary = new LoanSummary(totalInterest, totalLoanCost, payOffDate);
+        return loanSummary;
+    }
 }
