be869390ca9cdb8d0a581d3ff55be42a5aaafb46
LiamParker1 <lpar161@aucklanduni.ac.nz>
2024-08-24 02:11:52 +1200

    Feature: Add table variables and set to correct properties

--- src/main/java/uoa/lavs/controllers/CustomerController.java
+++ src/main/java/uoa/lavs/controllers/CustomerController.java
@@ -2,15 +2,125 @@ package uoa.lavs.controllers;
 
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TabPane;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
+import javafx.scene.control.cell.PropertyValueFactory;
 import org.springframework.stereotype.Controller;
 import uoa.lavs.Main;
 import uoa.lavs.SceneManager.AppScene;
+import uoa.lavs.models.Address;
+import uoa.lavs.models.Email;
+import uoa.lavs.models.Employer;
+import uoa.lavs.models.Loan;
+import uoa.lavs.models.Phone;
 
 @Controller
 public class CustomerController {
+  @FXML private Button homeButton;
+  @FXML private Button saveButton;
+  @FXML private Button cancelButton;
+  @FXML private Button editButton;
+
+  @FXML private Label titleLabel;
+  @FXML private Label customerIdLabel;
+
+  @FXML private TabPane detailsTabPane;
+
+  @FXML private TextField titleField;
+  @FXML private TextField nameField;
+  @FXML private DatePicker dobPicker;
+  @FXML private TextField occupationField;
+  @FXML private TextField citizenshipField;
+  @FXML private TextField visaField;
+
+  @FXML private TextArea notesArea;
+
+  @FXML private Button addAddressButton;
+  @FXML private TableView<Address> addressTable;
+  @FXML private TableColumn<Address, String> addressTypeColumn;
+  @FXML private TableColumn<Address, String> streetAddressColumn;
+  @FXML private TableColumn<Address, String> suburbColumn;
+  @FXML private TableColumn<Address, String> cityColumn;
+  @FXML private TableColumn<Address, String> postcodeColumn;
+  @FXML private TableColumn<Address, String> countryColumn;
+  @FXML private TableColumn<Address, String> primaryAddressColumn;
+  @FXML private TableColumn<Address, String> mailingAddressColumn;
+
+  @FXML private Button addEmailButton;
+  @FXML private TableView<Email> emailTable;
+  @FXML private TableColumn<Email, String> emailAddressColumn;
+  @FXML private TableColumn<Email, String> primaryEmailColumn;
+
+  @FXML private Button addPhoneButton;
+  @FXML private TableView<Phone> phoneTable;
+  @FXML private TableColumn<Phone, String> phoneTypeColumn;
+  @FXML private TableColumn<Phone, String> prefixColumn;
+  @FXML private TableColumn<Phone, String> phoneNumberColumn;
+  @FXML private TableColumn<Phone, String> primaryPhoneColumn;
+  @FXML private TableColumn<Phone, String> textingPhoneColumn;
+
+  @FXML private Button addEmploymentButton;
+  @FXML private TableView<Employer> employmentTable;
+  @FXML private TableColumn<Employer, String> nameColumn;
+  @FXML private TableColumn<Employer, String> employmentStreetAddressColumn;
+  @FXML private TableColumn<Employer, String> employmentPhoneNumberColumn;
+  @FXML private TableColumn<Employer, String> employmentEmailAddressColumn;
+  @FXML private TableColumn<Employer, String> websiteColumn;
+  @FXML private TableColumn<Employer, String> ownerColumn;
+
+  @FXML private Button addLoanButton;
+  @FXML private TableView<Loan> loanTable;
+  @FXML private TableColumn<Loan, String> loanIdColumn;
+  @FXML private TableColumn<Loan, String> statusColumn;
+  @FXML private TableColumn<Loan, String> principalColumn;
+  @FXML private TableColumn<Loan, String> startDateColumn;
+  @FXML private TableColumn<Loan, String> paymentFrequencyColumn;
+
   @FXML
   public void initialize() {
-    System.out.println("CustomerController initialized");
+    addressTypeColumn.setCellValueFactory(new PropertyValueFactory<Address, String>("type"));
+    streetAddressColumn.setCellValueFactory(
+        new PropertyValueFactory<Address, String>("streetAddress"));
+    suburbColumn.setCellValueFactory(new PropertyValueFactory<Address, String>("suburb"));
+    cityColumn.setCellValueFactory(new PropertyValueFactory<Address, String>("city"));
+    postcodeColumn.setCellValueFactory(new PropertyValueFactory<Address, String>("postCode"));
+    countryColumn.setCellValueFactory(new PropertyValueFactory<Address, String>("country"));
+    primaryAddressColumn.setCellValueFactory(
+        new PropertyValueFactory<Address, String>("isPrimary"));
+    mailingAddressColumn.setCellValueFactory(
+        new PropertyValueFactory<Address, String>("isMailing"));
+
+    emailAddressColumn.setCellValueFactory(new PropertyValueFactory<Email, String>("address"));
+    primaryEmailColumn.setCellValueFactory(new PropertyValueFactory<Email, String>("isPrimary"));
+
+    phoneTypeColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("type"));
+    prefixColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("prefix"));
+    phoneNumberColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("phoneNumber"));
+    primaryPhoneColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("isPrimary"));
+    textingPhoneColumn.setCellValueFactory(new PropertyValueFactory<Phone, String>("canSendText"));
+
+    nameColumn.setCellValueFactory(new PropertyValueFactory<Employer, String>("name"));
+    employmentStreetAddressColumn.setCellValueFactory(
+        new PropertyValueFactory<Employer, String>("streetAddress"));
+    employmentPhoneNumberColumn.setCellValueFactory(
+        new PropertyValueFactory<Employer, String>("phoneNumber"));
+    employmentEmailAddressColumn.setCellValueFactory(
+        new PropertyValueFactory<Employer, String>("emailAddress"));
+    websiteColumn.setCellValueFactory(new PropertyValueFactory<Employer, String>("website"));
+    ownerColumn.setCellValueFactory(new PropertyValueFactory<Employer, String>("isOwner"));
+
+    loanIdColumn.setCellValueFactory(new PropertyValueFactory<Loan, String>("loanId"));
+    statusColumn.setCellValueFactory(new PropertyValueFactory<Loan, String>("status"));
+    principalColumn.setCellValueFactory(new PropertyValueFactory<Loan, String>("principal"));
+    startDateColumn.setCellValueFactory(new PropertyValueFactory<Loan, String>("startDate"));
+    paymentFrequencyColumn.setCellValueFactory(
+        new PropertyValueFactory<Loan, String>("paymentFrequency"));
   }
 
   @FXML
@@ -46,6 +156,11 @@ public class CustomerController {
   @FXML
   private void onClickSave(ActionEvent event) {
     System.out.println("Save clicked");
+  }
+
+  @FXML
+  private void onClickEdit(ActionEvent event) {
+    System.out.println("Edit clicked");
   }
 
   @FXML
