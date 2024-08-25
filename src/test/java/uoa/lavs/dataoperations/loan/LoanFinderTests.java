99bcdc166226d15173fc039a0a28c112d2626c34
victor <vqiu25@gmail.com>
2024-08-26 09:59:41 +1200

    Feature: Mainframe unit tests for LoanFinder

---
+++ src/test/java/uoa/lavs/dataoperations/loan/LoanFinderTests.java
@@ -0,0 +1,65 @@
+package uoa.lavs.dataoperations.loan;
+
+import static org.junit.jupiter.api.Assertions.assertAll;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.List;
+import org.junit.jupiter.api.Test;
+import uoa.lavs.dataoperations.DataOperationsTestsHelper;
+import uoa.lavs.models.Loan;
+
+public class LoanFinderTests {
+
+  @Test
+  public void findExistingLoanFromDatabase() throws Exception {}
+
+  @Test
+  public void findNonExistingLoanFromDatabase() throws Exception {}
+
+  @Test
+  public void findExistingLoanFromMainframe() throws Exception {
+    DataOperationsTestsHelper.createTestingDatabases();
+    String customerId = "123";
+
+    List<Loan> loans = LoanFinder.findData(customerId);
+
+    assertAll(
+        () -> assertEquals("123", loans.get(0).getCustomerId()),
+        () -> assertEquals("123-09", loans.get(0).getLoanId()));
+  }
+
+  @Test
+  public void findNonExistingLoanFromMainframe() throws Exception {
+    DataOperationsTestsHelper.createTestingDatabases();
+    String customerId = "invalid-id";
+
+    assertThrows(
+        Exception.class,
+        () -> {
+          LoanFinder.findFromMainframe(customerId);
+        });
+  }
+
+  @Test
+  public void findExistingLoan() throws Exception {
+    DataOperationsTestsHelper.createTestingDatabases();
+    String customerId = "123";
+
+    List<Loan> loans = LoanFinder.findData(customerId);
+
+    assertAll(
+        () -> assertEquals("123", loans.get(0).getCustomerId()),
+        () -> assertEquals("123-09", loans.get(0).getLoanId()));
+  }
+
+  @Test
+  public void findNonExistingLoan() throws Exception {
+    DataOperationsTestsHelper.createTestingDatabases();
+    String customerId = "3";
+
+    List<Loan> phones = LoanFinder.findData(customerId);
+    assertTrue(phones.isEmpty());
+  }
+}
