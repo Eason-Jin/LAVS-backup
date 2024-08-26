33e759f24d0397a146061d66bf8ac7568af0e4b4
Jessica Jiang <jessicaj2947@gmail.com>
2024-08-26 20:40:14 +1200

    Refactor: Fix tests

--- src/test/java/uoa/lavs/models/AddressTests.java
+++ src/test/java/uoa/lavs/models/AddressTests.java
@@ -3,8 +3,6 @@ package uoa.lavs.models;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-import java.util.List;
-
 public class AddressTests {
 
     @Test
@@ -14,38 +12,6 @@ public class AddressTests {
         address.setLine2("Apt 4B");
         String expected = "123 Main St Apt 4B";
         String actual = address.getStreetAddress();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getListRepresentation() {
-        Address address = new Address(
-            "cust123",
-            "Home",
-            "123 Main St",
-            "Apt 4B",
-            "Suburbia",
-            "Metropolis",
-            "12345",
-            "Countryland",
-            true,
-            false,
-            1
-        );
-
-        List<String> expected = List.of(
-            "Home",
-            "123 Main St",
-            "Apt 4B",
-            "Suburbia",
-            "Metropolis",
-            "12345",
-            "Countryland",
-            "true",
-            "false"
-        );
-
-        List<String> actual = address.getListRepresentation();
         assertEquals(expected, actual);
     }
 }
--- src/test/java/uoa/lavs/models/EmployerTests.java
+++ src/test/java/uoa/lavs/models/EmployerTests.java
@@ -2,8 +2,6 @@ package uoa.lavs.models;
 
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
-import java.util.List;
-
 import org.junit.jupiter.api.Test;
 
 public class EmployerTests {
@@ -15,40 +13,6 @@ public class EmployerTests {
         employer.setLine2("Apt 4B");
         String expected = "123 Main St Apt 4B";
         String actual = employer.getStreetAddress();
-        assertEquals(expected, actual);
-    }
-
-    @Test
-    public void getListRepresentation() {
-        Employer employer = new Employer(
-                "cust123",
-                "Tech Corp",
-                "123 Tech St",
-                "Suite 100",
-                "Tech Suburb",
-                "Tech City",
-                "12345",
-                "Tech Country",
-                "123-456-7890",
-                "contact@techcorp.com",
-                "www.techcorp.com",
-                true,
-                1);
-
-        List<String> expected = List.of(
-                "Tech Corp",
-                "123 Tech St",
-                "Suite 100",
-                "Tech Suburb",
-                "Tech City",
-                "12345",
-                "Tech Country",
-                "123-456-7890",
-                "contact@techcorp.com",
-                "www.techcorp.com",
-                "true");
-
-        List<String> actual = employer.getListRepresentation();
         assertEquals(expected, actual);
     }
 }
