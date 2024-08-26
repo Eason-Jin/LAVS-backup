4d533c0884758282d1830d494f6c8a0f6abcbb12
LiamParker1 <lpar161@aucklanduni.ac.nz>
2024-08-26 16:08:15 +1200

    Refactor: Remove unnecessary print statements

--- src/main/java/uoa/lavs/controllers/Controller.java
+++ src/main/java/uoa/lavs/controllers/Controller.java
@@ -120,7 +120,6 @@ public abstract class Controller {
       TextArea ta = (TextArea) str;
       if (ta.getText() == null || ta.getText().length() > length) {
         ta.getStyleClass().add("invalid");
-        System.out.println(ta.getStyleClass());
         return true;
       }
     }
