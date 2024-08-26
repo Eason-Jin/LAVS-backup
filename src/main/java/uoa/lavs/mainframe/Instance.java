8bc9e63f7001fc7ed5e5b2b9f21b4630d1822655
Jessica Jiang <jessicaj2947@gmail.com>
2024-08-26 22:05:39 +1200

    Refactor: Reset instance

--- src/main/java/uoa/lavs/mainframe/Instance.java
+++ src/main/java/uoa/lavs/mainframe/Instance.java
@@ -1,29 +1,20 @@
 package uoa.lavs.mainframe;
 
-import java.nio.file.Files;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.sql.DriverManager;
-import java.sql.SQLException;
-
-import uoa.lavs.mainframe.simulator.HttpConnection;
 import uoa.lavs.mainframe.simulator.NitriteConnection;
-import uoa.lavs.mainframe.simulator.SimpleReplayConnection;
 
 // implements the singleton pattern for a mainframe connection
 public class Instance {
   // private constructor so that this class can only be initialized internally
-  private Instance() {}
+  private Instance() {
+  }
 
   // the path to the data file
-  private static final String dataPath = "http://localhost:5000/";
-
-  private static boolean _useTestConnections;
+  private static final String dataPath = "lavs-data.txt";
 
   // internal class to initialize the singleton, this enables lazy-loading
   // for the singleton
   private static class SingletonHelper {
-    private static Connection INSTANCE = new HttpConnection(dataPath);
+    private static Connection INSTANCE = new NitriteConnection(dataPath);
   }
 
   // return the underlying connection
